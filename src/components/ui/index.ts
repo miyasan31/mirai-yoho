fd6d4d4cac0a6029171db1c0ff2becf7c4e1d248
miyasan31 <miyasan.dev@gmail.com>
2026-03-24 01:40:25 +0900

    Add new UI components and styling recipes for Badge, CloseButton, Dialog, Field, IconButton, Icon, Input, Menu, Select, Skeleton, Text, Textarea, Toaster, and Tooltip. Integrate Panda CSS for consistent styling and structure across components, enhancing the overall UI framework.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,11 +1,33 @@
 export { AbsoluteCenter, type AbsoluteCenterProps } from "./absolute-center";
+export { Badge, type BadgeProps } from "./badge";
 export {
   Button,
   ButtonGroup,
   type ButtonGroupProps,
   type ButtonProps,
 } from "./button";
+export { CloseButton, type CloseButtonProps } from "./close-button";
+export * as Dialog from "./dialog";
+export * as Field from "./field";
 export { Group, type GroupProps } from "./group";
+export { Icon, type IconProps } from "./icon";
+export { IconButton, type IconButtonProps } from "./icon-button";
+export { Input, type InputProps } from "./input";
 export { Loader, type LoaderProps } from "./loader";
+export * as Menu from "./menu";
+export * as Select from "./select";
+export {
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+  type SkeletonCircleProps,
+  type SkeletonProps,
+  type SkeletonTextProps,
+} from "./skeleton";
 export { Span, type SpanProps } from "./span";
 export { Spinner, type SpinnerProps } from "./spinner";
+export * as Table from "./table";
+export { Text, type TextProps } from "./text";
+export { Textarea, type TextareaProps } from "./textarea";
+export { Toaster, toaster } from "./toast";
+export { Tooltip, type TooltipProps } from "./tooltip";
--- src/theme/recipes/index.ts
+++ src/theme/recipes/index.ts
@@ -1,3 +1,16 @@
+import { table } from "./table";
+import { text } from "./text";
+import { tooltip } from "./tooltip";
+import { menu } from "./menu";
+import { dialog } from "./dialog";
+import { select } from "./select";
+import { textarea } from "./textarea";
+import { input } from "./input";
+import { field } from "./field";
+import { skeleton } from "./skeleton";
+import { toast } from "./toast";
+import { icon } from "./icon";
+import { badge } from "./badge";
 import { spinner } from "./spinner";
 import { absoluteCenter } from "./absolute-center";
 import { group } from "./group";
@@ -7,5 +20,18 @@ export const recipes = {
   group,
   absoluteCenter,
   spinner,
+  badge,
+  icon,
+  toast,
+  skeleton,
+  field,
+  input,
+  textarea,
+  select,
+  dialog,
+  menu,
+  tooltip,
+  text,
+  table,
 };
 export const slotRecipes = {};
