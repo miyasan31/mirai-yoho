a57c2a2cdaa58ca0041781805d20d00f81110af7
miyasan31 <miyasan.dev@gmail.com>
2026-03-24 01:25:41 +0900

    Add Panda CSS configuration and update project files for integration

---
+++ panda.config.ts
@@ -0,0 +1,111 @@
+import { green } from "@/theme/colors/green";
+import { red } from "@/theme/colors/red";
+import { slate } from "@/theme/colors/slate";
+import { blue } from "@/theme/colors/blue";
+import { animationStyles } from "@/theme/animation-styles";
+import { zIndex } from "@/theme/tokens/z-index";
+import { shadows } from "@/theme/tokens/shadows";
+import { durations } from "@/theme/tokens/durations";
+import { colors } from "@/theme/tokens/colors";
+import { textStyles } from "@/theme/text-styles";
+import { layerStyles } from "@/theme/layer-styles";
+import { keyframes } from "@/theme/keyframes";
+import { globalCss } from "@/theme/global-css";
+import { conditions } from "@/theme/conditions";
+import { slotRecipes, recipes } from "@/theme/recipes";
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  preflight: true,
+
+  include: [
+    "./src/app/**/*.{ts,tsx,js,jsx}",
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+  ],
+
+  jsxFramework: "react",
+  exclude: [],
+
+  theme: {
+    extend: {
+      animationStyles: animationStyles,
+      recipes: recipes,
+      slotRecipes: slotRecipes,
+      keyframes: keyframes,
+      layerStyles: layerStyles,
+      textStyles: textStyles,
+
+      tokens: {
+        colors: colors,
+        durations: durations,
+        zIndex: zIndex,
+      },
+
+      semanticTokens: {
+        colors: {
+          fg: {
+            default: {
+              value: {
+                _light: "{colors.gray.12}",
+                _dark: "{colors.gray.12}",
+              },
+            },
+
+            muted: {
+              value: {
+                _light: "{colors.gray.11}",
+                _dark: "{colors.gray.11}",
+              },
+            },
+
+            subtle: {
+              value: {
+                _light: "{colors.gray.10}",
+                _dark: "{colors.gray.10}",
+              },
+            },
+          },
+
+          border: {
+            value: {
+              _light: "{colors.gray.4}",
+              _dark: "{colors.gray.4}",
+            },
+          },
+
+          error: {
+            value: {
+              _light: "{colors.red.9}",
+              _dark: "{colors.red.9}",
+            },
+          },
+
+          blue: blue,
+          gray: slate,
+          red: red,
+          green: green,
+        },
+
+        shadows: shadows,
+
+        radii: {
+          l1: {
+            value: "{radii.xs}",
+          },
+
+          l2: {
+            value: "{radii.sm}",
+          },
+
+          l3: {
+            value: "{radii.md}",
+          },
+        },
+      },
+    },
+  },
+
+  outdir: "styled-system",
+  globalCss: globalCss,
+  conditions: conditions,
+});
